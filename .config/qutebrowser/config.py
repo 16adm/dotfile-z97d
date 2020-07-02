ac3ad87ca82400115f496db6f2f418f2969bec52
16adm <phillippe.shieh@gmail.com>
2020-07-02 15:55:43 +0800

    Changes to be committed: 	modified:   .config/fcitx/rime/build/bopomofo.prism.bin 	modified:   .config/fcitx/rime/build/bopomofo_tw.prism.bin 	modified:   .config/fcitx/rime/build/cangjie5.prism.bin 	modified:   .config/fcitx/rime/build/cangjie5.reverse.bin 	modified:   .config/fcitx/rime/build/cangjie5.table.bin 	modified:   .config/fcitx/rime/build/luna_pinyin.prism.bin 	modified:   .config/fcitx/rime/build/luna_pinyin.reverse.bin 	modified:   .config/fcitx/rime/build/luna_pinyin.table.bin 	modified:   .config/fcitx/rime/build/luna_pinyin_fluency.prism.bin 	modified:   .config/fcitx/rime/build/luna_pinyin_simp.prism.bin 	modified:   .config/fcitx/rime/build/luna_quanpin.prism.bin 	modified:   .config/fcitx/rime/build/stroke.prism.bin 	modified:   .config/fcitx/rime/build/stroke.reverse.bin 	modified:   .config/fcitx/rime/build/stroke.table.bin 	modified:   .config/fcitx/rime/build/terra_pinyin.prism.bin 	modified:   .config/fcitx/rime/build/terra_pinyin.reverse.bin 	modified:   .config/fcitx/rime/build/terra_pinyin.table.bin 	modified:   .config/fcitx/rime/luna_pinyin.userdb/CURRENT 	modified:   .config/fcitx/rime/luna_pinyin.userdb/LOG 	modified:   .config/fcitx/rime/user.yaml 	modified:   .config/mpv/input.conf 	modified:   .config/mpv/mpv.conf 	modified:   .config/pcmanfm/default/pcmanfm.conf 	deleted:    .config/qutebrowser/autoconfig.yml 	modified:   .config/qutebrowser/config.py 	modified:   .config/yay/config.json 	deleted:    bin/wp

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,11 +1,44 @@
+# Appearance 
 import gruvboxmaterial.draw
-
 # Load existing settings made via :set
 config.load_autoconfig()
-
+# Gruvbox Material Theme
 gruvboxmaterial.draw.blood(c, {
     'spacing': {
         'vertical': 6,
         'horizontal': 8
     }
 })
+# Font
+c.fonts.default_family = "M+ 1mn"
+c.fonts.default_size = "12pt"
+
+# General
+c.downloads.location.directory = "~/Downloads"
+c.url.start_pages = ["https://github.com"] 
+c.url.default_page = "about:blank"
+c.url.searchengines = {"DEFAULT": "https://duckduckgo.com/search?q={}",
+                        "aw": "https://wiki.archlinux.org/?search={}",
+                        "bb": "https://search.bilibili.com/all?keyword={}",
+                        "bn": "https://www.bing.com/search?q={}",
+                        "cw": "https://zh.wikipedia.org/wiki/{}",
+                        "da": "https://www.deviantart.com/search?q={}",
+                        "dict": "http://dict.cn/{}",
+                        "ew": "https://en.wikipedia.org/wiki/{}",
+                        "gg": "https://google.com/search?q={}",
+                        "git": "https://github.com/search?q={}",
+                        "quo": "https://www.quora.com/search?q={}",
+                        "rd": "https://reddit.com/search/?q={}",
+                        "rdr": "https://reddit.com/r/{}",
+                        "sos": "https://stackoverflow.com/search?q={}",
+                        "yt": "https://www.youtube.com/results?search_query={}",
+                        "zh": "https://www.zhihu.com/search?q={}",
+                        }
+
+# Key Binding
+config.bind("am", "spawn -v mpv {url}")
+config.bind("aM", "hint links spawn -v mpv {hint-url}")
+config.bind("ay", "spawn -v youtube-dl {url}")
+config.bind("aY", "hint links spawn -v youtube-dl {hint-url}")
+config.bind("B", "set-cmd-text -s :bookmark-load")
+
